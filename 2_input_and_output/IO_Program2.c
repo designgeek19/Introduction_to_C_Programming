d7da4e75be4437af03cfaf202b31b4fd8847d040
designgeek19 <pauladeka3@gmail.com>
2023-03-22 13:37:38 +0100

    added comments // on code

--- 2_input_and_output/IO_Program2.c
+++ 2_input_and_output/IO_Program2.c
@@ -5,7 +5,12 @@ int main(void)
 	char f,m,l;
 	int age;
 
-	scanf("%c %c %c %d", &f, &m, &l, &age);
-	printf("My full name initials are: %c%c%c and my age is %d.\n",f,m,l,age);
+	printf("Enter your initials, followed by your age: "); //Initial prompt for the user
+	scanf("%c %c %c %d", &f, &m, &l, &age); //Reads whatever the user enters after the prompt (must be an integer/digit)
+	printf("My full name initials are: %c%c%c and my age is %d.\n",f,m,l,age); //The final output
 	return(0);
 } 
+// After the program is run
+// The first printf statement prompts the user to enter their full name initials
+// The scanf statement then reads the inputed character by the user
+// Finally, the second printf statement prints the output to the user
--- 2_input_and_output/IO_scanf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int y;
-{
-	printf("Enter a value for y: ");
-	scanf("%d", &y);
-	printf("You entered: %d\n", y);
-}
